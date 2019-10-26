c68e2b0585eb218ca6de17e4ea9d645175ec1328
lnasrullah <57022961+lnasrullah@users.noreply.github.com>
2019-10-26 19:17:11 +0700

    mencetak gambar

---
+++ 3.php
@@ -0,0 +1,14 @@
+<?php
+
+echo " --- panjang ---<br>";
+$n= 5;
+for($i=1;$i<=$n;$i++)
+
+        {
+
+            if($i % 2 == 0)
+                {echo "* = = = *<br>";}
+            else
+                {echo "* * * * *<br>";}
+        }
+?>
